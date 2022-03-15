07931109ed70eb39040f956877d197dd72c45254
venkateshsunkari <98217595+venkateshsunkari@users.noreply.github.com>
2022-03-15 16:09:30 +0530

    Update snapdeal.tf

--- snapdeal.tf
+++ snapdeal.tf
@@ -3,9 +3,11 @@ provider "aws" {
   access_key = "AKIAVMFWVN35PU6A6M4R"
   secret_key = "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"
 }
-resource "aws_opsworks_rds_db_instance" "my_instance" {
-  stack_id            = aws_opsworks_stack.my_stack.id
-  rds_db_instance_arn = aws_db_instance.my_instance.arn
-  db_user             = "admin"
-  db_password         = "admin@123456"
+resource "aws_elasticache_cluster" "ramu" {
+  cluster_id           = "flipkart"
+  engine               = "redis"
+  node_type            = "cache.t2.micro"
+  num_cache_nodes      = 1
+  parameter_group_name = "default.redis"
+  port                 = "6379"
 }
